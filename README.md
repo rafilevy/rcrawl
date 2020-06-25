295f46b2ffdadec8a30f71ca326db510faaea8e9
Rafi Levy <rafi.levy@sky.com>
2020-06-25 21:08:53 +0100

    v1.1.2 prints to stdout on find rather than at end

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# rcrawl
+# rcrawl 1.1.2
 
 A command line program which recursively searches for a named file or directory - built with rust
 (Similar to the UNIX find command)
 
-```_
 Recursively searches for a given file or directory and prints its full path to stdout
 
+```_
 USAGE:
-    rcrawl [FLAGS] [OPTIONS] <dirname>
+    rcrawl [FLAGS] [OPTIONS] <filename>
 
 FLAGS:
-    -f, --full       A flag indicating that full rather than relative paths should be returned
-    -h, --help       Prints help information
-    -s, --single     A flag indicating only the first found item should be output (equivalent to --max_items 1)
-    -V, --version    Prints version information
+    -h, --help        Prints help information
+    -r, --relative    A flag indicating that relative rather than full files paths should be returned
+    -s, --single      A flag indicating only the first found item should be output (equivalent to --max_items 1)
+    -V, --version     Prints version information
 
 OPTIONS:
     -d, --max_depth <max_depth>    The maximum depth to recursively search to
     -i, --max_items <max_items>    The maximum number of results to return
 
 ARGS:
-    <dirname>    The name of the file or directory to search for
+    <filename>    The name of the file/directory to search for
 ```
--- src/rcrawl.rs
+++ src/rcrawl.rs
@@ -1,17 +1,15 @@
 use clap::{ArgMatches};
 use std::collections::{VecDeque};
 use std::path::{PathBuf};
-use std::fs;
-use std::env;
 
 const DEFAULT_MAX_DEPTH: u8 = 50;
 const DEFAULT_MAX_ITEMS: u8 = 10;
 
 pub struct Config {
-    full: bool,
+    relative: bool,
     max_items: u8,
     max_depth: u8,
-    query: String,
+    match_expr: String,
 }
 
 impl Config {
@@ -20,78 +18,94 @@ impl Config {
             Some(i) => String::from(i).parse().unwrap(),
             None => DEFAULT_MAX_DEPTH
         };
-        let query: String = matches.value_of("dirname").unwrap_or_default().to_owned();
+        let match_expr: String = matches.value_of("filename").unwrap_or_default().to_owned();
         let max_items: u8 = if matches.is_present("single") { 1 } else {
             match matches.value_of("max_items") {
                 Some(i) => String::from(i).parse().unwrap(),
                 None => DEFAULT_MAX_ITEMS
             }
         };
-        let full = matches.is_present("full");
-        Config { max_depth, query, max_items, full }
+        let relative = matches.is_present("relative");
+        Config { max_depth, match_expr, max_items, relative }
     }
 }
 
 struct DirNode {
-    path: Box<PathBuf>,
+    path: PathBuf,
     depth: u8
 }
 
-fn find_dir(name: &str, max_depth: u8, max_items: u8) -> std::io::Result<Vec<PathBuf>> {
+struct FileSearch<'a> {
+    match_expr: &'a str,
+    max_depth: u8,
 
-    let mut results: Vec<PathBuf> = Vec::new();
-    let mut found = 0;
+    queue: VecDeque<DirNode>
+}
 
-    let mut queue: VecDeque<DirNode> = VecDeque::new();
+impl<'a> FileSearch<'a> {
+    fn new(match_expr: &'a str, root_dir: PathBuf, max_depth: u8) -> std::io::Result<FileSearch<'a>> {
+        let mut queue = VecDeque::new();
+        queue.push_front(DirNode {
+            path: root_dir,
+            depth: 0
+        });
+        Ok(FileSearch { match_expr, max_depth, queue })
+    }
+}
 
-    queue.push_front(DirNode {
-        path: Box::new(env::current_dir()?),
-        depth: 0
-    });
+impl Iterator for FileSearch<'_> {
 
-    let mut current: DirNode;
-    while !queue.is_empty() && found < max_items {
-        current = queue.pop_front().unwrap();
-        if current.depth < max_depth && current.path.is_dir() {
-            for entry in std::fs::read_dir(current.path.to_path_buf())? {
-                let entry : fs::DirEntry = entry?;
-                if entry.path().file_name().unwrap().eq(name) {
-                    results.push(entry.path().to_path_buf());
-                    found += 1;
-                    if found == max_items { break; }
+    type Item = PathBuf;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut current: DirNode;
+        while !self.queue.is_empty() {
+            current = self.queue.pop_front().unwrap();
+            if current.depth < self.max_depth && current.path.is_dir() {
+                let dir_contents = std::fs::read_dir(&current.path);
+                match dir_contents {
+                    Ok(read_dir) => {
+                        for entry in read_dir {
+                            let entry = entry.unwrap();
+                            if entry.path().file_name().unwrap() == self.match_expr {
+                                return Some(entry.path());
+                            }
+                            self.queue.push_front(DirNode {
+                                path: entry.path(),
+                                depth: current.depth + 1,
+                            });
+                        }
+                    },
+                    Err(_) => {
+                        eprintln!("rcrawl: Permission denied for {:?}", current.path)
+                    }
                 }
-                queue.push_front(DirNode {
-                    path: Box::new(entry.path()),
-                    depth: current.depth + 1,
-                });
             }
-        }
-    };
-
-    Ok(results)
+        };
+        None
+    }
 }
 
 pub fn run(cfg: Config) -> std::io::Result<()> {
-    let results = find_dir(&cfg.query, cfg.max_depth, cfg.max_items)?;
-    if results.is_empty() {
-        eprintln!("rcrawl: No files or directory {} was found", cfg.query)
-    } else {
-        let cwd = env::current_dir().unwrap();
-        for result in results {
-            let path = if cfg.full { result }
-            else {
-                let mut cmp_path = PathBuf::new();
-                let mut path = PathBuf::new();
-                for part in result.iter() {
-                    cmp_path.push(part);
-                    if cmp_path.gt(&cwd) {
-                        path.push(part);
-                    }
-                }
-                path
-            };
-            println!("{}", path.to_str().unwrap().replace(" ", "\\ "))
-        }
+    let root_dir = std::env::current_dir()?;
+    let mut count: u8 = 0;
+    for result in FileSearch::new(&cfg.match_expr, root_dir, cfg.max_depth)? {
+        if count == cfg.max_items { break; }
+        let path = result;
+        // let path = if !cfg.relative { result }
+        // else {
+        //     let mut cmp_path = PathBuf::new();
+        //     let mut path = PathBuf::new();
+        //     for part in result.iter() {
+        //         cmp_path.push(part);
+        //         if cmp_path.gt(&root_dir) {
+        //             path.push(part);
+        //         }
+        //     }
+        //     path
+        // };
+        println!("{}", path.to_str().unwrap().replace(" ", "\\ "));
+        count += 1;
     }
     Ok(())
 }
