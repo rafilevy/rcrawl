3c3d83f99ac7cdc01cbb258d585066f228a4f7b0
Rafi Levy <rafi.levy@sky.com>
2021-10-10 19:14:17 +0100

    Added verbose, root_dir and hidden file options

--- src/rcrawl.rs
+++ src/rcrawl.rs
@@ -1,6 +1,6 @@
 use clap::{ArgMatches};
 use std::collections::{VecDeque};
-use std::path::{PathBuf};
+use std::path::{PathBuf, Path};
 use std::io::Result;
 use std::thread;
 use std::sync::{Arc, Mutex};
@@ -13,11 +13,14 @@ const DEFAULT_MAX_ITEMS: u32 = u32::MAX;
 const DEFAULT_NUM_THREADS: u8 = 16;
 
 pub struct Config {
+    verbose: bool,
     relative: bool,
+    all: bool,
 
     max_items: u32,
     max_depth: u8,
 
+    root_dir: String,
     match_expr: String,
     regex: bool,
 
@@ -31,6 +34,7 @@ impl Config {
             None => DEFAULT_MAX_DEPTH
         };
         let match_expr: String = matches.value_of("filename").unwrap_or_default().to_owned();
+        let root_dir: String = matches.value_of("root_directory").unwrap_or_default().to_owned();
         let max_items: u32 = if matches.is_present("single") { 1 } else {
             match matches.value_of("max_items") {
                 Some(i) => String::from(i).parse().unwrap(),
@@ -43,7 +47,9 @@ impl Config {
             None => DEFAULT_NUM_THREADS
         };
         let regex: bool = matches.is_present("regex");
-        Config { max_depth, match_expr, regex, max_items, relative, num_threads }
+        let verbose: bool = matches.is_present("verbose");
+        let all: bool = matches.is_present("all");
+        Config { max_depth, match_expr, regex, max_items, relative, num_threads, verbose, root_dir, all }
     }
 }
 
@@ -62,6 +68,9 @@ struct ConcurrentFileSearch<'a> {
     max_depth: u8,
     max_items: u32,
     relative: bool,
+    root_dir: &'a Path,
+    verbose: bool,
+    all: bool,
     num_threads: u8,
     regex: bool,
 
@@ -69,19 +78,19 @@ struct ConcurrentFileSearch<'a> {
 }
 
 impl<'a> ConcurrentFileSearch<'a> {
-    fn new(match_expr: &'a str, root_dir: PathBuf, max_depth: u8, max_items: u32, relative: bool, num_threads: u8, regex: bool) -> Result<ConcurrentFileSearch<'a>> {
+    fn new(match_expr: &'a str, root_dir: &'a Path, max_depth: u8, max_items: u32, relative: bool, num_threads: u8, regex: bool, verbose: bool, all: bool) -> Result<ConcurrentFileSearch<'a>> {
         let search_queue = Arc::new(Mutex::new(VecDeque::new()));
         search_queue.lock().unwrap().push_front(DirNode {
             depth: 0,
-            path: root_dir
+            path: root_dir.to_path_buf()
         });
-        Ok(ConcurrentFileSearch {match_expr, regex, max_depth, max_items, relative, search_queue, num_threads})
+        Ok(ConcurrentFileSearch {match_expr, root_dir, regex, max_depth, max_items, relative, search_queue, num_threads, verbose, all})
     }
 }
 
 impl ConcurrentFileSearch<'_> {
     fn search(&mut self) {
-        let path_printer = Arc::new(PathPrinter::new(self.relative).unwrap());
+        let path_printer = Arc::new(PathPrinter::new(self.relative, self.root_dir).unwrap());
         let mut handles: Vec<std::thread::JoinHandle<()>> = vec!();
         let num_items : Arc<Mutex<u32>> = Arc::new(Mutex::new(0));
         let match_expr = Arc::new(if self.regex {
@@ -92,6 +101,7 @@ impl ConcurrentFileSearch<'_> {
         } else {
             MatchExpr::String(self.match_expr.to_owned())
         });
+        let verbose = self.verbose;
         for _ in 0..self.num_threads {
             let search_queue = Arc::clone(&self.search_queue);
             let path_printer = Arc::clone(&path_printer);
@@ -99,6 +109,7 @@ impl ConcurrentFileSearch<'_> {
             let match_expr = Arc::clone(&match_expr);
             let max_depth = self.max_depth;
             let max_items = self.max_items;
+            let all = self.all;
             let handle = thread::spawn(move || {
                 while (*num_items.lock().unwrap() < max_items) && !(*search_queue.lock().unwrap()).is_empty() {
                     let current = {
@@ -114,7 +125,9 @@ impl ConcurrentFileSearch<'_> {
                         Ok(read_dir) => {
                             for entry in read_dir {
                                 let entry = entry.unwrap();
-                                if !entry.file_name().as_os_str().to_str().unwrap().starts_with(".") {
+                                let file_name = entry.file_name();
+                                let entry_str = file_name.to_str().unwrap();
+                                if all || (!entry_str.starts_with(".") && !entry_str.eq("Library")) {
                                     (search_queue.lock().unwrap()).push_back(DirNode {
                                         path: entry.path(),
                                         depth: current.depth + 1,
@@ -132,7 +145,7 @@ impl ConcurrentFileSearch<'_> {
                             }
                         },
                         Err(e) => {
-                            if e.kind() == std::io::ErrorKind::PermissionDenied {
+                            if verbose && e.kind() == std::io::ErrorKind::PermissionDenied {
                                 eprintln!("Permission denied for: {:?}", current.path)
                             }
                         }
@@ -148,7 +161,7 @@ impl ConcurrentFileSearch<'_> {
 }
 
 pub fn run(cfg: Config) -> std::io::Result<()> {
-    let root_dir = std::env::current_dir()?;
-    ConcurrentFileSearch::new(&cfg.match_expr, root_dir, cfg.max_depth, cfg.max_items, cfg.relative, cfg.num_threads, cfg.regex)?.search();
+    let root_dir = std::fs::canonicalize(cfg.root_dir)?;
+    ConcurrentFileSearch::new(&cfg.match_expr, &root_dir, cfg.max_depth, cfg.max_items, cfg.relative, cfg.num_threads, cfg.regex, cfg.verbose, cfg.all)?.search();
     Ok(())
 }
